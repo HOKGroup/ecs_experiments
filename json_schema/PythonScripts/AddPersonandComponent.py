dd6d47e3b59116a1367b3ec42e794898526cb027
greg.schleusner@hok.com <greg.schleusner@Hok.com>
2023-01-26 16:31:59 -0500

    Added Python Scripts

---
+++ json_schema/PythonScripts/AddPersonandComponent.py
@@ -0,0 +1,70 @@
+import sqlite3
+from sqlite3 import Error
+
+
+def create_connection(db_file):
+    """ create a database connection to the SQLite database
+        specified by db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+    except Error as e:
+        print(e)
+
+    return conn
+
+
+def create_entity(conn, entity):
+    """
+    Create a new entity into the entity table
+    :param conn:
+    :param entity:
+    :return: entitycreate
+    """
+    sql = ''' INSERT INTO entity(classification,classification_reference,context,creation_date)
+              VALUES(?,?,?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, entity)
+    conn.commit()
+    return cur.lastrowid
+
+
+def create_component(conn, component):
+    """
+    Create a new component
+    :param conn:
+    :param component:
+    :return: 
+    """
+
+    sql = ''' INSERT INTO component(context,component_name,component_id,entity_guid,entity_classification,component_type,component_type_referenece,component_payload_type,owner,version,status,active,creation_date,authoring_application,hash1,schema,payload)
+              VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, component)
+    conn.commit()
+    return cur.lastrowid
+
+
+def main():
+    database = r"B:\Git\ecs_experiments\json_schema\PythonScripts\ecs.db"
+
+    # create a database connection
+    conn = create_connection(database)
+    with conn:
+        # create a new entity
+        enitity = ('Person', 'http...', 'hok-staff-active', '20220403');
+        entity_guid = create_entity(conn, enitity)
+
+        # component
+        component1 = ('hok-staff-active', 'Steve Smith',"1", entity_guid, 'Person', 'person.details', 'http...', 'json', 'HOK' ,0 ,'looking good', 1, '20220403' , 'HOK People APP' ,'jdjdjdjdjd' ,'person.details.v01', 'some json')
+
+        # create componnet
+        create_component(conn, component1)
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ json_schema/PythonScripts/CreateECSSqlite.py
@@ -0,0 +1,105 @@
+import sqlite3
+from sqlite3 import Error
+
+
+def create_connection(db_file):
+    """ create a database connection to the SQLite database
+        specified by db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except Error as e:
+        print(e)
+
+    return conn
+
+
+def create_table(conn, create_table_sql):
+    """ create a table from the create_table_sql statement
+    :param conn: Connection object
+    :param create_table_sql: a CREATE TABLE statement
+    :return:
+    """
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
+    except Error as e:
+        print(e)
+
+def main():
+    database = r"B:\Git\ecs_experiments\json_schema\PythonScripts\ecs.db"
+
+    sql_create_entity_table = """ CREATE TABLE IF NOT EXISTS entity (
+                                        entity_guid integer PRIMARY KEY AUTOINCREMENT,
+                                        classification text NOT NULL,
+                                        classification_reference text,
+                                        context text,
+                                        creation_date text
+                                    ); """
+
+    sql_create_component_table = """CREATE TABLE IF NOT EXISTS component (
+                                        component_guid integer PRIMARY KEY AUTOINCREMENT,
+                                        context text,
+                                        component_name text,
+                                        component_id text,
+                                        entity_guid text,
+                                        entity_classification text,
+                                        component_type text,
+                                        component_type_referenece text,
+                                        component_payload_type text,
+                                        owner text,
+                                        version integer,
+                                        status text,
+                                        active integer,
+                                        creation_date text,
+                                        authoring_application text,
+                                        hash1 text,
+                                        schema text,
+                                        payload blob,
+                                        FOREIGN KEY (entity_guid) REFERENCES entity (entity_guid)
+                                    );"""
+    
+    sql_create_relationship_table = """CREATE TABLE IF NOT EXISTS relationship (
+                                        relationship_guid integer PRIMARY KEY AUTOINCREMENT,
+                                        context text
+                                    );"""
+    
+    sql_create_layer_table = """CREATE TABLE IF NOT EXISTS layer (
+                                        layer_guid integer PRIMARY KEY AUTOINCREMENT,
+                                        context text
+                                    );"""
+    
+    sql_create_scene_table = """CREATE TABLE IF NOT EXISTS scene (
+                                        scene_guid integer PRIMARY KEY AUTOINCREMENT,
+                                        context text
+                                    );"""
+
+    # create a database connection
+    conn = create_connection(database)
+
+    # create tables
+    if conn is not None:
+        # create projects entity
+        create_table(conn, sql_create_entity_table)
+
+        # create tasks component
+        create_table(conn, sql_create_component_table)
+
+        # create tasks table
+        create_table(conn, sql_create_relationship_table)
+
+        # create tasks table
+        create_table(conn, sql_create_layer_table)
+
+        # create tasks table
+        create_table(conn, sql_create_scene_table)
+    else:
+        print("Error! cannot create the database connection.")
+
+
+if __name__ == '__main__':
+    main()
