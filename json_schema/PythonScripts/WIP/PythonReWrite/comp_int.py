632ec0eb4242fcf23f2c8ada122b0e536151ba1a
gschleusner1972 <gschleusner@gmail.com>
2023-02-04 10:10:02 -0500

    Refactor Add Data to one script
    Added Spec
    
    Still need to add component sequence to the rest of the entities.
    
    Merged Relationships add data script

---
+++ json_schema/PythonScripts/WIP/PythonReWrite/comp_int.py
@@ -0,0 +1,15 @@
+class persondetails:
+    def __init__(self,firstname, lastname, email,uniqueid):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.email = email
+        self.unqiueid = uniqueid
+
+class persondetails:
+    def __init__(self,firstname, lastname, email,uniqueid):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.email = email
+        self.unqiueid = uniqueid
+
+
