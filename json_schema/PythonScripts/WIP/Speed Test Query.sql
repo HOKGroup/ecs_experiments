bf156c14dcacfb20aa8d8a4e5591e40b6a0eb220
gschleusner1972 <gschleusner@gmail.com>
2023-02-04 13:29:41 -0500

    Tests of JSON queries in SQLite

---
+++ json_schema/PythonScripts/WIP/Speed Test Query.sql
@@ -0,0 +1,9 @@
+select json_extract(payload, '$.Address1') as address1
+from component
+WHERE col_12 = "value14"
+
+
+select json_extract(payload, '$.Address1') as address1
+from component
+WHERE col_12 = "value14"
+AND json_extract(payload,'$.Address1')='mhDr1TO4HK5ml161r0YxPjSOMF9pBOiHWJrlwvqTtfUAKPRXyhgdNnIHrnyJ7yzXnnsFQPDB7FVRBUKaqHcacnry06ZHKWVVkMPU'
