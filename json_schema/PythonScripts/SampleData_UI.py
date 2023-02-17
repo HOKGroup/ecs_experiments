c7a2ae9f136fee884ed71ea282358a558e8a7971
gschleusner1972 <gschleusner@gmail.com>
2023-02-17 09:07:22 -0500

    Making Sample Data

--- json_schema/PythonScripts/SampleData_UI.py
+++ json_schema/PythonScripts/SampleData_UI.py
@@ -4,6 +4,13 @@ import os
 import uuid
 from datetime import datetime
 import random
+import string
+
+class FileOption:
+    def __init__(self, parent, options):
+        self.parent = parent
+        self.options = options
+        self.file_path = None
 
 class CompanyDetailsForm:
     def __init__(self, master):
@@ -13,7 +20,7 @@ class CompanyDetailsForm:
         self.schema_files = {
             "entity": "json_schema/Entity and Component Definitions/entity.json",
             "component": "json_schema/Entity and Component Definitions/component.json",
-            "payload": "json_schema\Entity and Component Definitions\project.ifc.full.json"
+            "payload": "json_schema\Entity and Component Definitions\company.serviceoffering.details.json"
         }
 
         self.schemas = {}
@@ -37,7 +44,7 @@ class CompanyDetailsForm:
                 # If the property has an enum, create a dropdown list box
                 if "enum" in options:
                     field = tk.StringVar(value=options["default"] if "default" in options else "")
-                    dropdown = tk.OptionMenu(frame, field, *options["enum"])
+                    dropdown = tk.OptionMenu(frame, field, *options["enum"],)
                     dropdown.pack()
                 # If the property is an array, create a multiline text box
                 elif options["type"] == "array":
@@ -53,15 +60,15 @@ class CompanyDetailsForm:
 
                 # If the property is sequence name
                 elif options["type"] == "string" and options.get("format") == "squence_name":
-                    field = tk.Entry(frame)
+                    field = tk.Entry(frame, width=60)
                     field.pack()
 
                     sequence_name = result_str = ''.join((random.choice('abcdxyzpqr') for i in range(5)))
                     field.insert(0, sequence_name)
                 
-                                # If the property is sequence number
+                # If the property is sequence number
                 elif options["type"] == "string" and options.get("format") == "squence_number":
-                    field = tk.Entry(frame)
+                    field = tk.Entry(frame, width=60)
                     field.pack()
 
                     sequence_number = random.randint(1, 10)
@@ -69,7 +76,7 @@ class CompanyDetailsForm:
 
                 # If the property is a UUID entity then insert UUID
                 elif options["type"] == "string" and options.get("format") == "uuid_entity":
-                    field = tk.Entry(frame)
+                    field = tk.Entry(frame, width=60, background="red")
                     field.pack()
 
                     uuid_ent = uuid.uuid4()
@@ -77,7 +84,7 @@ class CompanyDetailsForm:
 
                 # If the property is a UUID component then insert UUID
                 elif options["type"] == "string" and options.get("format") == "uuid_component":
-                    field = tk.Entry(frame)
+                    field = tk.Entry(frame, width=60, bg="#856ff8")
                     field.pack()
 
                     uuid_comp = uuid.uuid4()
@@ -85,7 +92,7 @@ class CompanyDetailsForm:
 
                 # If the property is a UUID_component_entity enter the UUID of the Entity
                 elif options["type"] == "string" and options.get("format") == "uuid_component_entity":
-                    field = tk.Entry(frame)
+                    field = tk.Entry(frame, width=60, background="red")
                     field.pack()
 
                     uuid_ent_comp = uuid_ent
@@ -97,7 +104,7 @@ class CompanyDetailsForm:
                     field.pack()
 
                 else:
-                    field = tk.Entry(frame)
+                    field = tk.Entry(frame, width=60)
                     field.pack()
 
                 fields[prop] = field
@@ -110,6 +117,11 @@ class CompanyDetailsForm:
 
 
     def save_data(self):
+        
+        # Create a Random String to add to the file name to identify complete datasets
+        random_string = ''.join(random.choices(string.ascii_lowercase + string.digits, k=8)) # Generate a random string of length 8
+        rand = random_string
+
         for name, schema in self.schemas.items():
             # Collect the data from the form fields and create a dictionary
             data = {}
@@ -123,16 +135,18 @@ class CompanyDetailsForm:
             # Determine the output file name based on the schema file name
             if name == "payload":
                 base_filename = os.path.splitext(os.path.basename(self.schema_files["payload"]))[0]
-                output_file = f"{base_filename}_data.json"
+
+                output_file = f"{base_filename}_payload_{rand}.json"
             else:
                 base_filename = os.path.splitext(os.path.basename(self.schema_files["payload"]))[0]
-                output_file = f"{base_filename}_{name}.json"
-                
+                output_file = f"{base_filename}_{name}_{rand}.json"
+
             output_path = os.path.join(os.path.dirname(self.schema_files["payload"]), output_file)
 
             # Write the data to a JSON file
             with open(output_path, "w") as f:
                 json.dump(data, f)
+
 
 root = tk.Tk()
 form = CompanyDetailsForm(root)
