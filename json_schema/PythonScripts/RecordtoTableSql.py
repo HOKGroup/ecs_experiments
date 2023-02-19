fe851f5a6a96ec2fc583c236c12b6f2a6fe38abd
gschleusner1972 <gschleusner@gmail.com>
2023-02-19 05:53:52 -0500

    Update Columner Sort

--- json_schema/PythonScripts/RecordtoTableSql.py
+++ json_schema/PythonScripts/RecordtoTableSql.py
@@ -1,6 +1,7 @@
 import sqlite3
 import random
 import string
+import uuid
 
 # Function to generate random string of given length
 def random_string(length):
@@ -12,34 +13,36 @@ conn = sqlite3.connect('sample_table.db')
 
 # Create a new table called "Component"
 create_table_query = '''CREATE TABLE Component (
-                            ID INTEGER PRIMARY KEY,
+                            ID  TEXT PRIMARY KEY,
+                            EntityGUID,
                             PropertyName TEXT,
                             PropertyValue TEXT
                         );'''
 conn.execute(create_table_query)
 
-# Insert 1000 rows with random text using 100 characters for each value
-insert_data_query = '''INSERT INTO Component (PropertyName, PropertyValue) VALUES (?, ?);'''
+# Prepare data to be inserted
 data = []
-for i in range(500000):
-    data.append(('ComponentGUID', random_string(100)))
-    data.append(('Context', random_string(100)))
-    data.append(('EntityGUID', random_string(100)))
-    data.append(('EntityClassification', random_string(100)))
-    data.append(('ComponentUserName', random_string(100)))
-    data.append(('ComponentUserID', random_string(100)))
-    data.append(('ComponentType', random_string(100)))
-    data.append(('ComponentTypeReference', random_string(100)))
-    data.append(('ComponentVersion', random_string(100)))
-    data.append(('ComponentStatus', random_string(100)))
-    data.append(('AuthorSoftware', random_string(100)))
-    data.append(('Hash1', random_string(100)))
-    data.append(('PayloadDataType', random_string(100)))
-    data.append(('Active', random_string(100)))
-    data.append(('DateCreated', random_string(100)))
+entity_guid = str(uuid.uuid4())
+for i in range(100):
+    data.append((entity_guid, 'ComponentGUID', random_string(100)))
+    data.append((entity_guid, 'Context', random_string(100)))
+    data.append((entity_guid, 'EntityClassification', random_string(100)))
+    data.append((entity_guid, 'ComponentUserName', random_string(100)))
+    data.append((entity_guid, 'ComponentUserID', random_string(100)))
+    data.append((entity_guid, 'ComponentType', random_string(100)))
+    data.append((entity_guid, 'ComponentTypeReference', random_string(100)))
+    data.append((entity_guid, 'ComponentVersion', random_string(100)))
+    data.append((entity_guid, 'ComponentStatus', random_string(100)))
+    data.append((entity_guid, 'AuthorSoftware', random_string(100)))
+    data.append((entity_guid, 'Hash1', random_string(100)))
+    data.append((entity_guid, 'PayloadDataType', random_string(100)))
+    data.append((entity_guid, 'Active', random_string(100)))
+    data.append((entity_guid, 'DateCreated', random_string(100)))
 
+# Insert data into the database
+insert_data_query = '''INSERT INTO Component (EntityGUID, PropertyName, PropertyValue) VALUES (?, ?, ?);'''
 conn.executemany(insert_data_query, data)
-
-# Commit changes and close the database connection
 conn.commit()
+
+# Close the database connection
 conn.close()
